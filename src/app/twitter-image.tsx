d51025a95687798c864cec307bc6b5547d64d035
nanaagyei <tuffourp@oregonstate.edu>
2026-07-05 18:23:30 -0500

    Add centralized SEO metadata infrastructure

---
+++ src/app/twitter-image.tsx
@@ -0,0 +1,12 @@
+import OgImage from "./opengraph-image";
+
+export const alt = "Akomapa Health - Building Ethical Global Health Leaders";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export const contentType = "image/png";
+
+export default function Image() {
+  return OgImage();
+}
